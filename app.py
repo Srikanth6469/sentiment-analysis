901ef71067148ee341e2f2c262a4e128a32ece8d
srikanth reddy <srikanthreddy@srikanths-MacBook-Air.local>
2025-06-28 21:59:27 +0530

    Apply latest changes

--- app.py
+++ app.py
@@ -302,7 +302,9 @@ def inslogin():
 
 # # -------------------------------Loginact End-----------------------------------------------------------------
 
+import os
 
-   
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Render dynamically sets this
+    app.run(debug=False, host='0.0.0.0', port=port)
+
--- app2.py
+++ app2.py
@@ -223,5 +223,8 @@ def convert(audio_path):
             return
         return target_path 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.1', port=10000)
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(debug=False, host='0.0.0.0', port=port)
